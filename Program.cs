383cc0983bc712d7730043162e7cc20cdb96d922
rushayhays <rushayhays@outlook.com>
2022-05-25 09:49:38 -0500

    name can be input

--- Program.cs
+++ Program.cs
@@ -41,7 +41,9 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            Console.WriteLine("What is your name Adventurer:");
+            string advName = Console.ReadLine();
+            Adventurer theAdventurer = new Adventurer(advName);
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.

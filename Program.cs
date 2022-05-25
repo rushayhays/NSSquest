23a3a6a1c2c7982e1681857e9e7a5d57d33a9aa5
rushayhays <rushayhays@outlook.com>
2022-05-25 09:59:41 -0500

    you can continue the quest if you want too

--- Program.cs
+++ Program.cs
@@ -45,36 +45,44 @@ namespace Quest
             string advName = Console.ReadLine();
             Adventurer theAdventurer = new Adventurer(advName);
 
-            // A list of challenges for the Adventurer to complete
-            // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
-            List<Challenge> challenges = new List<Challenge>()
-            {
-                twoPlusTwo,
-                theAnswer,
-                whatSecond,
-                guessRandom,
-                favoriteBeatle
-            };
+            bool doYouWantToContinue = true;
+            while(doYouWantToContinue){
+                // A list of challenges for the Adventurer to complete
+                // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
+                List<Challenge> challenges = new List<Challenge>()
+                {
+                    twoPlusTwo,
+                    theAnswer,
+                    whatSecond,
+                    guessRandom,
+                    favoriteBeatle
+                };
 
-            // Loop through all the challenges and subject the Adventurer to them
-            foreach (Challenge challenge in challenges)
-            {
-                challenge.RunChallenge(theAdventurer);
-            }
+                // Loop through all the challenges and subject the Adventurer to them
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
 
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
-            if (theAdventurer.Awesomeness >= maxAwesomeness)
-            {
-                Console.WriteLine("YOU DID IT! You are truly awesome!");
-            }
-            else if (theAdventurer.Awesomeness <= minAwesomeness)
-            {
-                Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-            }
-            else
-            {
-                Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                // This code examines how Awesome the Adventurer is after completing the challenges
+                // And praises or humiliates them accordingly
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                    Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+                Console.WriteLine("Do you want to Continue(y/n)");
+                string contAnswer = Console.ReadLine();
+                if(contAnswer == "n"){
+                    doYouWantToContinue = false;
+                }
             }
         }
     }
